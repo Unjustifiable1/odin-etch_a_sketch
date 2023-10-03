1c1cbbd7b04f9b81786d08683b922f67f853dad0
Unjustifiable1 <cupidojustin7@gmail.com>
2023-10-03 16:41:20 +0200

    grid sized by slider

--- mainscript.js
+++ mainscript.js
@@ -1,18 +1,42 @@
-const gridSize = 3;
+// Set grid size with slider
+const gridSlider = document.getElementById('gridSize');
+const output = document.getElementById('gridValue');
+let gridSize = gridSlider.value;
+output.innerHTML = gridSize + " x " + gridSize;
+
+gridSlider.oninput = function() {
+    gridSize = this.value;
+    output.innerHTML = gridSize + " x " + gridSize;
+    gridResize();
+    return;
+}
+
+
+// resize sketch grid
 const r = document.querySelector(':root');
-r.style.setProperty('--grid-size', gridSize);
-
-
 const sketchGrid = document.querySelector("#sketchGrid");
 
-// resize grid
 function gridResize() {
+    removeSketchGrid();
+    r.style.setProperty('--grid-size', gridSize);
     for (let i = 1; i <= gridSize * gridSize; i++) {
         const sketchBlock = document.createElement('div');
         sketchBlock.setAttribute('id', 'sketchBlock');
-        sketchBlock.textContent = i;
+        // sketchBlock.textContent = i;
         sketchGrid.appendChild(sketchBlock);
     }
 }
 
+
+// remove sketch grid divs
+function removeSketchGrid() {
+    let child = sketchGrid.lastElementChild;
+    while (child) {
+        sketchGrid.removeChild(child);
+        child = sketchGrid.lastElementChild;
+    }
+}
+
+
+// run sketch grid sizing immediately on page load
 gridResize();
